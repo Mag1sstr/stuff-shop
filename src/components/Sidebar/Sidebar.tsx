5bfa4df0b0bf848a8506afb4b39928bb090784fd
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-05-21 20:07:59 +0500

    create Sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,34 @@
+import { Link, NavLink } from "react-router-dom";
+import { useGetCategoriesQuery } from "../../api/api";
 import styles from "./Sidebar.module.scss";
+import { ROUTES } from "../../utils/routes";
 
 function Sidebar() {
+  const { data: categories } = useGetCategoriesQuery(null);
+
   return (
-    <div>
-      <p>sidebar</p>
-    </div>
+    <section className={styles.sidebar}>
+      <div className={styles.col}>
+        <h3 className={styles.title}>CATEGORIES</h3>
+
+        <ul className={styles.links}>
+          {categories?.map(({ name, id }) => (
+            <li key={id} className={styles.link}>
+              <NavLink
+                className={({ isActive }) => (isActive ? styles.active : "")}
+                to={ROUTES.CATEGORIES + `/${id}`}
+              >
+                {name[0].toUpperCase() + name.slice(1).toLowerCase()}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.help}>
+        Help
+        <span>Terms & Conditions</span>
+      </div>
+    </section>
   );
 }
 

178fa7c87b7d10aad908c0d961f16c81b0decde2
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-05-25 23:49:56 +0500

    update Search

--- src/components/Header/Search/Search.tsx
+++ src/components/Header/Search/Search.tsx
@@ -1,15 +1,49 @@
 import styles from "./Search.module.scss";
 import search from "../../../images/header/search.svg";
+import type { IProducts } from "../../../types/interfaces";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../../utils/routes";
 
-function Search() {
+interface IProps {
+  searchValue: string;
+  setSearchValue: (value: string) => void;
+  data: IProducts[];
+}
+
+function Search({ searchValue, setSearchValue, data }: IProps) {
   return (
     <div className={styles.search}>
       <img src={search} alt="" />
       <input
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
         className={styles.input}
         type="text"
         placeholder="Search for anything..."
       />
+      <div className={`${styles.drop} ${searchValue.trim() && styles.open}`}>
+        {data?.length ? (
+          data.map(({ images, title, category, id }) => (
+            <Link
+              key={id}
+              onClick={() => setSearchValue("")}
+              to={ROUTES.PRODUCTS + `/${id}`}
+            >
+              <div className={styles.item}>
+                <div className={styles.row}>
+                  <img className={styles.img} src={images[0]} alt="" />
+                  <div>
+                    <p className={styles.title}>{title}</p>
+                    <p className={styles.category}>{category.name}</p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))
+        ) : (
+          <div>Not found</div>
+        )}
+      </div>
     </div>
   );
 }

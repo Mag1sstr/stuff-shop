2d634efc71c4c1cd58960de844d31e79a0d78ced
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-05-26 02:59:08 +0500

    Update AppRouter.tsx

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../Home/Home";
+import SingleProduct from "../../pages/SingleProduct/SingleProduct";
+import { ROUTES } from "../../utils/routes";
 
 function AppRouter() {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path={`${ROUTES.PRODUCTS}/:id`} element={<SingleProduct />} />
     </Routes>
   );
 }

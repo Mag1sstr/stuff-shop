61d0e15a3eaa831ef71f36ec6b902d9a8981adfe
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-05-27 03:32:49 +0500

    create Poster

---
+++ src/components/Poster/Poster.tsx
@@ -0,0 +1,27 @@
+import Button from "../Button/Button";
+import styles from "./Poster.module.scss";
+function Poster() {
+  return (
+    <section className={styles.wrapper}>
+      <div className="container">
+        <div className={styles.poster}>
+          <div className={styles.left}>
+            <h2 className={styles.title}>
+              NEW YEAR <br /> <span>SALE</span>
+            </h2>
+            <div className={styles.btn}>
+              <Button title="See more" />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <p className={styles.text}>
+              save up to <span>50%</span> off
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Poster;

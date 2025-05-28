9038e545120114f16442616ad19b2de069ed8450
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-05-29 00:51:31 +0500

    create SingleProduct

---
+++ src/pages/SingleProduct/SingleProductBanner/SingleProductBanner.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import type { IProducts } from "../../../types/interfaces";
+import styles from "./SingleProductBanner.module.scss";
+import Button from "../../../components/Button/Button";
+import Spinner from "../../../components/Spinner/Spinner";
+
+interface IProps extends IProducts {
+  isLoading: boolean;
+}
+
+function SingleProductBanner({
+  images,
+  title,
+  description,
+  price,
+  category,
+  isLoading,
+}: IProps) {
+  const [currentImage, setCurrentImage] = useState(0);
+  return (
+    <div className={styles.banner}>
+      {isLoading ? (
+        <Spinner center />
+      ) : (
+        <div className={styles.row}>
+          <div className={styles.img__block}>
+            <img className={styles.img} src={images[currentImage]} alt="" />
+            <div className={styles.col}>
+              {images.map((img, index) => (
+                <img
+                  onClick={() => setCurrentImage(index)}
+                  className={styles.item}
+                  src={img}
+                  alt=""
+                />
+              ))}
+            </div>
+          </div>
+          <div className={styles.info}>
+            <h3 className={styles.title}>{title}</h3>
+            <p className={styles.price}>{price}$</p>
+            <p className={styles.feature}>
+              Category: <span>{category.name}</span>
+            </p>
+            <p className={styles.description}>{description}</p>
+            <div className={styles.buttons}>
+              <Button title="Add to cart" />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SingleProductBanner;

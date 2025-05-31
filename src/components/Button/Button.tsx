480e6b186d1bf2e90f281ba216097f6a2158abae
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-05-31 06:08:41 +0500

    Update Button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,20 @@
 import styles from "./Button.module.scss";
 
 interface IProps {
-  title: string;
+  children?: React.ReactNode;
   onClick?: () => void;
   fontWeight?: number;
   width?: string | number;
 }
 
-function Button({ title, onClick, fontWeight = 600, width }: IProps) {
+function Button({ children, onClick, fontWeight = 600, width }: IProps) {
   return (
     <button
       onClick={onClick}
       className={styles.btn}
       style={{ fontWeight, width }}
     >
-      {title}
+      {children}
     </button>
   );
 }

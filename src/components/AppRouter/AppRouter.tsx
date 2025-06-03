353dba2108783cf6c49e2043df57d1468492a1ef
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-06-03 21:15:41 +0500

    Update AppRouter.tsx

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -3,12 +3,14 @@ import Home from "../../pages/Home/Home";
 import SingleProduct from "../../pages/SingleProduct/SingleProduct";
 import { ROUTES } from "../../utils/routes";
 import Cart from "../../pages/Cart/Cart";
+import CategoryProducts from "../../pages/CategoryProducts/CategoryProducts";
 
 function AppRouter() {
   return (
     <Routes>
       <Route index element={<Home />} />
       <Route path={`${ROUTES.PRODUCTS}/:id`} element={<SingleProduct />} />
+      <Route path={`${ROUTES.CATEGORIES}/:id`} element={<CategoryProducts />} />
       <Route path={ROUTES.CART} element={<Cart />} />
     </Routes>
   );

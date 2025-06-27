f1d0e4add2f670fe976cb344ccdeeaf01ccd9fe6
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-06-27 07:24:20 +0500

    fix adaptive User & Header

--- src/components/Header/User/User.tsx
+++ src/components/Header/User/User.tsx
@@ -1,5 +1,6 @@
 import styles from "./User.module.scss";
 import avatar from "../../../images/header/avatar.svg";
+import icon from "../../../images/header/user-icon.svg";
 
 interface IProps {
   onClick?: () => void;
@@ -8,6 +9,8 @@ interface IProps {
 function User({ onClick }: IProps) {
   return (
     <div onClick={onClick} className={styles.user}>
+      <img className={styles.icon} src={icon} alt="user" />
+
       <div
         className={styles.avatar}
         style={{ backgroundImage: `url(${avatar})` }}
